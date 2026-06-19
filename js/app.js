8c2d3c62b4d98a0e5f39b198459a58be28603294
vinojenavs <oajenavi@bupathway.edu>
2026-06-19 11:23:28 +0100

    cardtemplate moved from search to app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import { searchTracks } from "./search.mjs";
-import { buildTrackCard } from "./search.mjs";
+//import { buildTrackCard } from "./search.mjs";
 const searchButton = document.getElementById("searchBtn");
 
 searchButton.addEventListener("click", async () => {
@@ -10,7 +10,21 @@ searchButton.addEventListener("click", async () => {
   list.innerHTML = "";
 
   tracks.forEach(track => {
-    const card = buildTrackCard(track);
+    const card = buildTrackTemplate(track);
     list.appendChild(card);
   });
 });
+
+function buildTrackTemplate(track) {
+  const card = document.createElement("div");
+  card.className = "track-card";
+  card.innerHTML = `
+    <a href="${track.external_urls.spotify}" target="_blank">
+    <img src="${track.album.images[0]?.url}" alt="Album Art" width="100"><br>
+    <p><strong>${track.name}</strong></p>
+    <p>Artist(s): ${track.artists.map(a => a.name).join(", ")}</p>
+    <p>Album: ${track.album.name}</p>
+    </a>
+  `;
+  return card;
+}
--- js/search.mjs
+++ js/search.mjs
@@ -1,9 +1,5 @@
 import { getToken } from "./auth.mjs";
 
-const clientID = "08d0df8ef27b4572adde7d940ac6b0b9";
-const clientSecret = "88753f9306f940d2a1a8d29a98871ba5";
-
-
 export async function searchTracks(query) {
   const token = await getToken();
   const result = await fetch(
@@ -14,17 +10,5 @@ export async function searchTracks(query) {
   return data.tracks.items;
 }
 
-export function buildTrackCard(track) {
-  const div = document.createElement("div");
-  div.className = "track-card";
-  div.innerHTML = `
-    <img src="${track.album.images[0]?.url}" alt="Album Art" width="100"><br>
-    <strong>${track.name}</strong><br>
-    Artist(s): ${track.artists.map(a => a.name).join(", ")}<br>
-    Album: ${track.album.name}<br>
-    Popularity: ${track.popularity}<br>
-    <a href="${track.external_urls.spotify}" target="_blank">Listen on Spotify</a>
-  `;
-  return div;
-}
 
+//Popularity: ${track.popularity}<br></br>
